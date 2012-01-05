69174f6621f0f02f63213bd16816926fa0edad14
Edward Caissie <edward.caissie@gmail.com>
2012-01-05 10:05:16 -0500

    First commit

---
+++ bns-body-classes.php
@@ -0,0 +1,79 @@
+<?php
+/*
+Plugin Name: BNS Body Classes
+Plugin URI: http://buynowshop.com/plugins/bns-body-classes/
+Description: Simple plugin that adds classes to the `body_class` output upon activation.
+Version: 0.1
+Text Domain: bns-bc
+Author: Edward Caissie
+Author URI: http://edwardcaissie.com/
+License: GNU General Public License v2
+License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+*/
+
+/**
+ * BNS Body Classes
+ *
+ * Simple plugin that adds classes to the `body_class` output upon activation.
+ *
+ * @package     BNS_Body_Classes
+ *
+ * @link        http://buynowshop.com/plugins/bns-body-classes/
+ * @link        https://github.com/Cais/bns-body-classes/
+ * @link        http://wordpress.org/extend/plugins/bns-body-classes
+ *
+ * @version     0.1
+ *
+ * @author      Edward Caissie <edward.caissie@gmail.com>
+ * @copyright   Copyright (c) 2012, Edward Caissie
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 2, as published by the
+ * Free Software Foundation.
+ *
+ * You may NOT assume that you can use any other version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to:
+ *
+ *      Free Software Foundation, Inc.
+ *      51 Franklin St, Fifth Floor
+ *      Boston, MA  02110-1301  USA
+ *
+ * The license for this software can also likely be found here:
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Last revised January 5, 2012
+ */
+
+/**
+ * BNS Body Classes
+ *
+ * Add additional classes to the core `body_class` function
+ *
+ * @package BNS_Body_Classes
+ * @since   0.1
+ *
+ * @param   $classes
+ *
+ * @return  array - $classes, an array of classes to be added to `body_class`
+ */
+add_filter( 'body_class', 'bns_body_classes' );
+function bns_body_classes( $classes ) {
+        /** Add self reference to default body classes; also serves as very simple example */
+        $classes[] = 'plugin-bns-body-classes';
+
+        /** Add child-theme-<Name> to default body classes */
+        if ( is_child_theme() ) {
+            $classes[] = 'child-theme-' . sanitize_html_class( get_option( 'stylesheet' ) );
+        }
+
+        /** Add theme-<Name> to default body classes */
+        $classes[] = 'theme-' . sanitize_html_class( get_option( 'template' ) );
+        return $classes;
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,66 @@
+=== BNS Body Classes ===
+Contributors: cais
+Donate Link: http://buynowshop.com/
+Tags: CSS, body
+Requires: tba
+Tested Up To: 3.3.1
+Stable Tag: 0.1
+
+Simple plugin that adds classes to the `body_class` output upon activation.
+
+== Description ==
+Simple plugin that adds classes to the `body_class` output upon activation.
+
+== Installation ==
+This section describes how to install the plugin and get it working.
+
+1. Go to the "Plugins" menu in the Administration Panels ("dashboard").
+2. Click the 'Add New' link.
+3. Click the "Upload" link.
+4. Browse for the bns-body-classes.zip file on your computer; upload; and, install accordingly.
+5. Activate.
+
+-- or -
+
+1. Go to the "Plugins" menu in the Administration Panels ("dashboard").
+2. Click the 'Add New' link.
+3. Search for BNS Body Classes.
+4. Install.
+5. Activate.
+
+Please read this article for further assistance: http://wpfirstaid.com/2009/12/plugin-installation/
+
+== Frequently Asked Question ==
+No questions have been asked.
+
+== Screenshots ==
+No screenshots are available.
+
+== Other Notes ==
+
+= Copyright 2012  Edward Caissie  (email : edward.caissie@gmail.com) =
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 2,
+  as published by the Free Software Foundation.
+
+  You may NOT assume that you can use any other version of the GPL.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+  The license for this software can also likely be found here:
+  http://www.gnu.org/licenses/gpl-2.0.html
+
+== Upgrade Notice ==
+Please stay current with your WordPress installation, your active theme, and your plugins.
+
+== Changelog ==
+= 0.1 =
+* Initial Release
