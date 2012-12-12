61951c31e86afc925ebc7a433013208181b64fef
Edward Caissie <edward.caissie@gmail.com>
2012-12-11 23:06:32 -0500

    More holidays added

--- bnsbc-2013.php
+++ bnsbc-2013.php
@@ -55,14 +55,86 @@ class BNSBC_2013 {
 
     /**
      * Holidays
+     * @internal Source: http://www.timeanddate.com/holidays/canada/2013
      */
     function Canadian_holidays() {
         $holidays = '';
         /** January */
         if ( '01' == date( 'm' ) ) {
-            if ( '02' == date( 'd' ) ) { $holidays .= ' quebec-day-after-new-years-day'; }
-            if ( '06' == date( 'd' ) ) { $holidays .= ' epiphany'; }
+            if ( '07' == date( 'd' ) ) { $holidays .= ' orthodox-christmas-day'; }
+            if ( '14' == date( 'd' ) ) { $holidays .= ' orthodox-new-year'; }
         }
+        /** February */
+        if ( '02' == date( 'm' ) ) {
+            if ( '02' == date( 'd' ) ) { $holidays .= ' groundhog-day'; }
+            if ( '10' == date( 'd' ) ) { $holidays .= ' chinese-new-year'; }
+            if ( '11' == date( 'd' ) ) { $holidays .= ' british-columbia family-day'; }
+            if ( '14' == date( 'd' ) ) { $holidays .= ' valentines-day'; }
+            if ( '18' == date( 'd' ) ) { $holidays .= ' manitoba louis-riel-day'; }
+            if ( '18' == date( 'd' ) ) { $holidays .= ' alberta ontario saskatchewan family-day'; }
+            if ( '18' == date( 'd' ) ) { $holidays .= ' prince-edward-island islander-day'; }
+            if ( '22' == date( 'd' ) ) { $holidays .= ' yukon heritage-day'; }
+            if ( '24' == date( 'd' ) ) { $holidays .= ' purim'; }
+        }
+        /** March */
+        if ( '03' == date( 'm' ) ) {
+            if ( '10' == date( 'd' ) ) { $holidays .= ' daylight-savings-time first-day'; }
+            if ( '17' == date( 'd' ) ) { $holidays .= ' st-patricks-day'; }
+            if ( '26' == date( 'd' ) ) { $holidays .= ' passover first-day'; }
+            if ( '29' == date( 'd' ) ) { $holidays .= ' good-friday'; }
+            if ( '30' == date( 'd' ) ) { $holidays .= ' holy-saturday'; }
+            if ( '31' == date( 'd' ) ) { $holidays .= ' easter-sunday'; }
+        }
+        /** April */
+        if ( '04' == date( 'm' ) ) {
+            if ( '01' == date( 'd' ) ) { $holidays .= ' easter-monday'; }
+            if ( '01' == date( 'd' ) ) { $holidays .= ' april-fools-day'; }
+            if ( '02' == date( 'd' ) ) { $holidays .= ' passover last-day'; }
+        }
+        /** May */
+        if ( '05' == date( 'm' ) ) {
+            if ( '03' == date( 'd' ) ) { $holidays .= ' orthodox-good-friday'; }
+            if ( '04' == date( 'd' ) ) { $holidays .= ' orthodox-holy-saturday'; }
+            if ( '05' == date( 'd' ) ) { $holidays .= ' orthodox-easter'; }
+            if ( '06' == date( 'd' ) ) { $holidays .= ' orthodox-easter-monday'; }
+            if ( '12' == date( 'd' ) ) { $holidays .= ' mothers-day'; }
+            if ( '15' == date( 'd' ) ) { $holidays .= ' shavuot'; }
+            if ( '20' == date( 'd' ) ) { $holidays .= ' victoria-day'; }
+        }
+        /** June */
+        if ( '06' == date( 'm' ) ) {
+            if ( '16' == date( 'd' ) ) { $holidays .= ' fathers-day'; }
+            if ( '24' == date( 'd' ) ) { $holidays .= ' quebec st-jean-baptiste-day'; }
+            if ( '24' == date( 'd' ) ) { $holidays .= ' newfoundland labrador discovery-day'; }
+        }
+        /** July */
+        if ( '07' == date( 'm' ) ) {
+            if ( '01' == date( 'd' ) ) { $holidays .= ' canada-day'; }
+            if ( '09' == date( 'd' ) ) { $holidays .= ' nunavut nunavut-day'; }
+            if ( '09' == date( 'd' ) ) { $holidays .= ' ramadan first-day'; }
+        }
+        /** August */
+        if ( '08' == date( 'm' ) ) {
+            if ( '05' == date( 'd' ) ) { $holidays .= ' ontario simcoe-day'; }
+            if ( '05' == date( 'd' ) ) { $holidays .= ' manitoba northwest-territories saskatchewan civic-holiday'; }
+            if ( '05' == date( 'd' ) ) { $holidays .= ' alberta heritage-day'; }
+            if ( '05' == date( 'd' ) ) { $holidays .= ' nova-scotia prince-edward-island natal-day'; }
+            if ( '05' == date( 'd' ) ) { $holidays .= ' british-columbia british-columbia-day'; }
+            if ( '19' == date( 'd' ) ) { $holidays .= ' yukon discovery-day'; }
+        }
+        /** September */
+        if ( '09' == date( 'm' ) ) {
+            if ( '02' == date( 'd' ) ) { $holidays .= ' labour-day'; }
+
+
+
+        }
+
+
+
+
+
+
         $holidays .= ' test-2013';
 
         return $holidays;
