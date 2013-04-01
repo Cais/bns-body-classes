fb17ce6e42d9615fcb9fb635dbc3b3644058b9cd
Edward Caissie <edward.caissie@gmail.com>
2013-04-01 09:22:46 -0400

    * Code formatting and code block termination comments

--- bnsbc-options.php
+++ bnsbc-options.php
@@ -14,7 +14,7 @@
  * @since       0.2
  *
  * @author      Edward Caissie <edward.caissie@gmail.com>
- * @copyright   Copyright (c) 2012, Edward Caissie
+ * @copyright   Copyright (c) 2012-2013, Edward Caissie
  *
  * Thanks to Samuel Wood aka Otto42 and his post at http://ottopress.com/2009/wordpress-settings-api-tutorial
  *
@@ -25,6 +25,10 @@
  * @version     0.4
  * @date        December 12, 2012
  * Refactored to more appropriate naming conventions
+ *
+ * @version     0.5
+ * @date        April 1, 2013
+ * Code formatting and code block termination comments
  *
  * @todo Add more options
  * @todo Sort out validations required (CSS class cannot start with a number, etc.)
@@ -47,22 +51,45 @@ function bnsbc_add_admin_page() {
         'bnsbc',
         'bnsbc_options_page'
     );
-}
+} /** End function - add admin page */
 add_action( 'admin_menu', 'bnsbc_add_admin_page' );
 
-// display the admin options page
+
+/**
+ * Options Page
+ * Display the admin options page
+ *
+ * @package     BNS_Body_Classes
+ * @subpackage  BNSBC_Options
+ *
+ * @uses        _e
+ * @uses        do_settings_sections
+ * @uses        esc_attr_e
+ * @uses        settings_fields
+ */
 function bnsbc_options_page() { ?>
+
     <div>
-        <h2><?php _e ( 'BNS Body Classes Options and Settings', 'bns-bc' ); ?></h2>
+        <h2>
+            <?php _e( 'BNS Body Classes Options and Settings', 'bns-bc' ); ?>
+        </h2>
+
         <?php _e( 'Options and settings related to the BNS Body Classes plugin.', 'bns-bc' ); ?>
+
         <form action="options.php" method="post">
+
             <?php
             settings_fields( 'bnsbc_custom_classes' );
             do_settings_sections( 'bnsbc' ); ?>
+
             <input name="Submit" type="submit" value="<?php esc_attr_e( 'Save Changes', 'bns-bc' ); ?>" />
-        </form>
+
+        </form><!-- End form -->
+
     </div>
-<?php }
+
+<?php } /** End function - options page */
+
 
 /**
  * BNSBC Admin Init
@@ -71,9 +98,10 @@ function bnsbc_options_page() { ?>
  * @package     BNS_Body_Classes
  * @subpackage  BNS_Options
  *
- * @uses        register_setting
- * @uses        add_settings_section
+ * @uses        __
  * @uses        add_settings_field
+ * @uses        add_settings_section
+ * @uses        register_setting
  */
 function bnsbc_admin_init() {
     /** Add Custom Classes Option */
@@ -95,28 +123,39 @@ function bnsbc_admin_init() {
         'bnsbc',
         'add_custom_classes'
     );
-}
+} /** End function - admin init */
 add_action( 'admin_init', 'bnsbc_admin_init' );
+
 
 /**
  * Add Custom Classes Text
  * Displays as a message for the input field
+ *
+ * @package     BNS_Body_Classes
+ * @subpackage  BNSBC_Options
+ *
+ * @uses        __
  */
 function add_custom_classes_text() {
     $text = 'Enter your custom classes into the text field and click the "Save Changes" button.';
     printf( __( '<p>%1$s</p>', 'bns-bc' ), $text );
-}
+} /** End function - add custom classes text */
+
 
 /**
  * Custom Classes Input
  * The input field for the custom classes
  *
- * @uses    get_option
+ * @package     BNS_Body_Classes
+ * @subpackage  BNSBC_Options
+ *
+ * @uses        get_option
  */
 function custom_classes_input() {
     $options = get_option( 'bnsbc_custom_classes_field' );
     echo "<input id='custom_classes_text_string' name='bnsbc_custom_classes_field[text_string]' size='40' type='text' value='{$options['text_string']}' />";
-}
+} /** End function - custom classes input */
+
 
 /**
  * BNSBC Custom Classes Validator
@@ -131,7 +170,8 @@ function bnsbc_custom_classes_validator( $input ) {
     $options = get_option( 'bnsbc_custom_classes_field' );
     $options['text_string'] = trim( $input['text_string'] );
     return $options;
-}
+} /** End function - custom classes validator */
+
 
 /**
  * BNS Body Classes Option Classes
@@ -159,8 +199,9 @@ function bnsbc_option_classes( $classes ) {
         $added_classes = preg_replace( '/\s\s+/', ' ', $added_classes );
         /** Add to $classes and return */
         $classes[] = $added_classes;
-    }
+    } /** End if - option classes */
 
     return $classes;
-}
+
+} /** End function - options classes */
 add_filter( 'body_class', 'bnsbc_option_classes' );
